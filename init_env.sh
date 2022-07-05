d394b9dcebb542fca843ec423b9f0dd12d9811f7
Jong-Hyuk Ellis Cho <ellisjoe611@gmail.com>
2022-07-06 00:51:52 +0900

    changed init_env.sh

--- init_env.sh
+++ init_env.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-LOCAL_HOSTNAME=$(curl http://169.254.169.254/latest/meta-data/local-hostname)
-echo "LOCAL_HOSTNAME=$LOCAL_HOSTNAME" > .env.prod
+PUBLIC_HOSTNAME=$(curl http://169.254.169.254/latest/meta-data/local-hostname)
+echo "PUBLIC_HOSTNAME=$PUBLIC_HOSTNAME" > .env.prod
 
-LOCAL_IPV4=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
-echo "LOCAL_IPV4=$LOCAL_IPV4" >> .env.prod
+PUBLIC_IPV4=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
+echo "PUBLIC_IPV4=$PUBLIC_IPV4" >> .env.prod
 
-VUE_APP_API_URL=http://${LOCAL_HOSTNAME}:3000/api/
+VUE_APP_API_URL=http://${PUBLIC_HOSTNAME}:3000/api/
 echo "VUE_APP_API_URL=$VUE_APP_API_URL" >> .env.prod
 
 REGION=$(curl http://169.254.169.254/latest/meta-data/placement/region)
