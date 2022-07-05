dcc05199203183c3963ba9ea68b8ba3c662eaee3
Jong-Hyuk Ellis Cho <ellisjoe611@gmail.com>
2022-07-06 00:24:18 +0900

    edited init_env.sh

--- init_env.sh
+++ init_env.sh
@@ -6,7 +6,7 @@ LOCAL_IPV4=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 echo "LOCAL_IPV4=$LOCAL_IPV4" >> .env.prod
 
 VUE_APP_API_URL=http://${LOCAL_HOSTNAME}:3000/api/
-echo "VUE_APP_API_URL=$LOCAL_HOSTNAME" > .env.prod
+echo "VUE_APP_API_URL=$LOCAL_HOSTNAME" >> .env.prod
 
 REGION=$(curl http://169.254.169.254/latest/meta-data/placement/region)
 echo "REGION=$REGION" >> .env.prod
