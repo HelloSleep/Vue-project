d065f03863e80eb775efd2b140b68be37b5e1b83
Jong-Hyuk Ellis Cho <ellisjoe611@gmail.com>
2022-07-06 00:22:03 +0900

    edited init_env.sh

--- init_env.sh
+++ init_env.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 LOCAL_HOSTNAME=$(curl http://169.254.169.254/latest/meta-data/local-hostname)
-echo "LOCAL_HOSTNAME=$LOCAL_HOSTNAME" > .env
+echo "LOCAL_HOSTNAME=$LOCAL_HOSTNAME" > .env.prod
 
 LOCAL_IPV4=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
-echo "LOCAL_IPV4=$LOCAL_IPV4" >> .env
+echo "LOCAL_IPV4=$LOCAL_IPV4" >> .env.prod
 
 VUE_APP_API_URL=http://${LOCAL_HOSTNAME}:3000/api/
-echo "VUE_APP_API_URL=$LOCAL_HOSTNAME" > .env
+echo "VUE_APP_API_URL=$LOCAL_HOSTNAME" > .env.prod
 
 REGION=$(curl http://169.254.169.254/latest/meta-data/placement/region)
-echo "REGION=$REGION" >> .env
+echo "REGION=$REGION" >> .env.prod
 
 AVAILABILITY_ZONE=$(curl http://169.254.169.254/latest/meta-data/placement/availability-zone)
-echo "AVAILABILITY_ZONE=$AVAILABILITY_ZONE" >> .env
+echo "AVAILABILITY_ZONE=$AVAILABILITY_ZONE" >> .env.prod
